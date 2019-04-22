9f97bcf39cfa848eb1746c44d73a5ab968541d42
jecueva1997 <reatiguijeferson@gmail.com>
2019-04-22 16:23:31 -0500

    ejercicios en clase

---
+++ demo2.py
@@ -0,0 +1,20 @@
+"""
+	archivo: demo2.py
+	Ejemplo de lenguaje de Python
+	autor: @reroes
+"""
+
+import sys
+variable = sys.argv[0]
+valor1 = sys.argv[1]
+valor2 = sys.argv[2]
+
+print("variable argv[0]:   %s" % variable)
+print("variable argv[1]:    %s" % dato1)
+print("variable argv[2]:    %s" % dato2)
+
+suma = int(valor1) + int(valor2) # aqui realizo la suma de las variables
+multiplicacion = int(valor1) * int(valor2) # aqui realizo la multiplicacion
+
+print ("La suma es: %s" suma)
+print ("La multiplicacion es: %s" % suma)
---
+++ demo3.py
@@ -0,0 +1,16 @@
+"""
+	archivo: demo2.py
+	Ejemplo de lenguaje de Python
+	autor: @reroes
+"""
+
+import sys
+valor1 = input("Ingrese el primer valor: ")
+valor2 = input("Ingrese el segundo valor: ")
+
+suma = int(valor1) + int(valor2)
+
+multiplicacion = int(valor1) * int(valor2)
+
+
+print ("La suma es %d :" suma )
